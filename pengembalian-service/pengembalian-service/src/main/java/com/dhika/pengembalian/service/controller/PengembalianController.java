35f344fe5763d5d619eb6d1c75f6341de6b732a1
Metri-Andhika-Pratama <113493886+Metri-Andhika-Pratama@users.noreply.github.com>
2022-11-02 13:40:30 +0700

    Add files via upload

---
+++ pengembalian-service/pengembalian-service/src/main/java/com/dhika/pengembalian/service/controller/PengembalianController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dhika.pengembalian.service.controller;
+
+import com.dhika.pengembalian.service.entity.Pengembalian;
+import com.dhika.pengembalian.service.service.PengembalianService;
+import com.dhika.pengembalian.service.vo.ResponseTemplateVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author User
+ */
+@RestController
+@RequestMapping("/pngembalian")
+public class PengembalianController {
+    @Autowired
+    private PengembalianService pengembalianService;
+    
+    @PostMapping("/")
+    public Pengembalian savePengembalian(@RequestBody Pengembalian pengembalian){
+        return pengembalian.savePengembalian(pengembalian);
+    }
+    
+    @GetMapping("{id}")
+    public ResponseTemplateVO getPengembalian(@PathVariable("id") Long pengembalianId){
+        return pengembalianService.getPengembalian(pengembalianId);
+    }
+}
