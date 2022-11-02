12b9d4170a45215854feaaddc11a4e4631a4f54f
Metri-Andhika-Pratama <113493886+Metri-Andhika-Pratama@users.noreply.github.com>
2022-11-02 13:15:54 +0700

    Add files via upload

---
+++ peminjaman-service/src/main/java/com/dhika/peminjaman/service/controller/PeminjamanController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dhika.peminjaman.service.controller;
+
+import com.dhika.peminjaman.service.entity.Peminjaman;
+import com.dhika.peminjaman.service.service.PeminjamanService;
+import com.dhika.peminjaman.service.vo.ResponseTemplateVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author User
+ */
+@RestController
+@RequestMapping("/peminjaman")
+public class PeminjamanController {
+    @Autowired
+    private PeminjamanService peminjamanService;
+    
+    @PostMapping("/")
+    public Peminjaman savePeminjaman(@RequestBody Peminjaman peminjaman){
+        return peminjamanService.savePeminjaman(peminjaman);
+    }
+    
+    @GetMapping("{id}")
+    public ResponseTemplateVO getPeminjaman(@PathVariable("id") Long peminjamanId){
+        return peminjamanService.getPeminjaman(peminjamanId);
+    }
+}
+
---
+++ peminjaman-service/src/main/java/com/dhika/peminjaman/service/vo/ResponseTemplateVO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dhika.peminjaman.service.vo;
+
+import com.dhika.peminjaman.service.entity.Peminjaman;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author User
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Peminjaman peminjaman;
+    private Anggota anggota;
+}
